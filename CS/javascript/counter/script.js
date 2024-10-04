fe912d6a5b3a8f4bd99c4b3aee67c269064f4f31
JoshuaTreeisplanterd <joshua.jordan@hgs.hiddengeniusproject.org>
2024-10-03 17:04:22 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,14 @@
-function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
-}
+function counter() 
+
+{
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+
+    // create a variable to hold the p tag we want to change the text of
+
+    // Convert the string of the p tag into a integer so you can add to it
+
+    // Update the text of the p tag with new value
+
+    }
+    counter_number= document.getElementById(counter_number)
+    counter_nmber.innerHTML=parseInt(counter_number.innerHTML)
